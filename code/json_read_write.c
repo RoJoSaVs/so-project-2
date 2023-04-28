f7b0ace42da5f19f35fac5a216b8e8aadaa54c41
Guillen00 <47611671+Guillen00@users.noreply.github.com>
2023-04-28 09:45:05 -0600

    lectura json

---
+++ code/json_read_write.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include "cJSON.h"
+#include "cJSON.c"
+
+cJSON *root;
+FILE *archivo;
+char nombre_archivo[100];
+
+int read(){
+    // Leer el archivo JSON
+    
+
+    archivo = fopen("test.json", "r");
+
+    if (archivo == NULL) {
+        printf("No se pudo abrir el archivo.\n");
+        return 1;
+    }
+
+    fseek(archivo, 0, SEEK_END);
+    long archivo_tamano = ftell(archivo);
+    rewind(archivo);
+
+    char *json_str = (char*) malloc(sizeof(char) * archivo_tamano);
+    fread(json_str, archivo_tamano, 1, archivo);
+
+    fclose(archivo);
+
+    // Parsear el archivo JSON
+    root = cJSON_Parse(json_str);
+
+    // Obtener el tamaño del arreglo
+    //int tamano = cJSON_GetArraySize(root);
+
+    // Crear un array de objetos JSON para almacenar los datos
+    //cJSON **datos = (cJSON**) malloc(sizeof(cJSON*) * tamano);
+
+    // Recorrer el arreglo y agregar los objetos al array
+    //int i;
+    //for (i = 0; i < tamano; i++) {
+    //    cJSON *objeto = cJSON_GetArrayItem(root, i);
+    //    datos[i] = objeto;
+    //}
+
+    // Imprimir los datos
+    //printf("Los datos del arreglo son:\n");
+    //for (i = 0; i < tamano; i++) {
+    //   cJSON *objeto = datos[i];
+    //    printf("Objeto %d:\n", i + 1);
+    //    printf("  Campo1: %s\n", cJSON_GetObjectItem(objeto, "server")->valuestring);
+    //    printf("  Campo2: %d\n", cJSON_GetObjectItem(objeto, "totalRequest")->valueint);
+    //}
+    //printf("\n");
+
+
+    return 0; 
+}
+
+int write(char const * name,int totalrequest, int timeExecution,int averageRequestTime, int memoryConsumption, int fourStat){
+     // Crear los objetos JSON
+    cJSON *obj1 = cJSON_CreateObject();
+    cJSON *arr = cJSON_CreateArray();
+
+    // Agregar los datos a los objetos JSON
+    cJSON_AddStringToObject(obj1, "server", name);
+    cJSON_AddNumberToObject(obj1, "totalRequest", totalrequest);
+    cJSON_AddNumberToObject(obj1, "timeExecution", timeExecution);
+    cJSON_AddNumberToObject(obj1, "averageRequestTime", averageRequestTime);
+    cJSON_AddNumberToObject(obj1, "memoryConsumption", memoryConsumption);
+    cJSON_AddNumberToObject(obj1, "fourStat", fourStat);
+
+    cJSON_AddItemToArray(root, obj1);
+
+    // Escribir el archivo JSON
+
+    archivo = fopen("test.json", "w");
+
+    if (archivo == NULL) {
+        printf("No se pudo abrir el archivo.\n");
+        return 1;
+    }
+
+    char *json_str = cJSON_Print(root);
+    fprintf(archivo, "%s", json_str);
+    fclose(archivo);
+}
+
+int save(char const * name,int totalrequest, int timeExecution,int averageRequestTime, int memoryConsumption, int fourStat){
+    read();
+    write(name,totalrequest,timeExecution,averageRequestTime,memoryConsumption,fourStat);
+}
+
