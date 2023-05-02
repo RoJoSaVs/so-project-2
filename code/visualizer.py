9339323d2a2980f0e4406d4457b28a821fb55308
Jess <jessicaes2000@gmail.com>
2023-05-01 23:26:25 -0600

    [UPDATE] Fixed format for y axis and fixed 4th graph

--- code/visualizer.py
+++ code/visualizer.py
@@ -2,6 +2,7 @@ import json
 
 import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 from pylab import *
 
 
@@ -19,45 +20,45 @@ def stringToJsonData(filename):
 def getStats(filename):
     jsonData = stringToJsonData(filename)
     fifoDict = {"totalRequest": [], "timeExecution": [], "averageRequestTime": [], "memoryConsumption": [],
-                "totalBytesSend": []}
+                "totalBytesSend": [0], "times": [0]}
     heavyDict = {"totalRequest": [], "timeExecution": [], "averageRequestTime": [], "memoryConsumption": [],
-                 "totalBytesSend": []}
+                 "totalBytesSend": [0], "times": [0]}
     threadDict = {"totalRequest": [], "timeExecution": [], "averageRequestTime": [], "memoryConsumption": [],
-                  "totalBytesSend": []}
+                  "totalBytesSend": [0], "times": [0]}
     preheavyDict = {"totalRequest": [], "timeExecution": [], "averageRequestTime": [], "memoryConsumption": [],
-                    "totalBytesSend": []}
+                    "totalBytesSend": [0], "times": [0]}
     for item in jsonData:
         if (item["server"] == "fifo"):
             fifoDict["totalRequest"].append(item["totalRequest"])
             fifoDict["timeExecution"].append(item["timeExecution"])
             fifoDict["averageRequestTime"].append(item["averageRequestTime"])
             fifoDict["memoryConsumption"].append(item["memoryConsumption"])
-            fifoDict["totalBytesSend"].append(
-                item["totalBytesSend"] / (item["totalRequest"] * item["timeExecution"] * 1000))
+            fifoDict["totalBytesSend"].append(item["totalBytesSend"])
+            fifoDict["times"].append((item["timeExecution"]) + fifoDict["times"][-1])
 
         elif (item["server"] == "heavy"):
             heavyDict["totalRequest"].append(item["totalRequest"])
             heavyDict["timeExecution"].append(item["timeExecution"])
             heavyDict["averageRequestTime"].append(item["averageRequestTime"])
             heavyDict["memoryConsumption"].append(item["memoryConsumption"])
-            heavyDict["totalBytesSend"].append(
-                item["totalBytesSend"] / (item["totalRequest"] * item["timeExecution"] * 1000))
+            heavyDict["totalBytesSend"].append(item["totalBytesSend"])
+            heavyDict["times"].append((item["timeExecution"]) + heavyDict["times"][-1])
 
         elif (item["server"] == "thread"):
             threadDict["totalRequest"].append(item["totalRequest"])
             threadDict["timeExecution"].append(item["timeExecution"])
             threadDict["averageRequestTime"].append(item["averageRequestTime"])
             threadDict["memoryConsumption"].append(item["memoryConsumption"])
-            threadDict["totalBytesSend"].append(
-                item["totalBytesSend"] / (item["totalRequest"] * item["timeExecution"] * 1000))
+            threadDict["totalBytesSend"].append(item["totalBytesSend"])
+            threadDict["times"].append((item["timeExecution"]) + threadDict["times"][-1])
 
         elif (item["server"] == "preheavy"):
             preheavyDict["totalRequest"].append(item["totalRequest"])
             preheavyDict["timeExecution"].append(item["timeExecution"])
             preheavyDict["averageRequestTime"].append(item["averageRequestTime"])
             preheavyDict["memoryConsumption"].append(item["memoryConsumption"])
-            preheavyDict["totalBytesSend"].append(
-                item["totalBytesSend"] / (item["totalRequest"] * item["timeExecution"] * 1000))
+            preheavyDict["totalBytesSend"].append(item["totalBytesSend"])
+            preheavyDict["times"].append((item["timeExecution"]) + preheavyDict["times"][-1])
 
     resultDict = {"fifoDict": fifoDict, "heavyDict": heavyDict,
                   "threadDict": threadDict, "preheavyDict": preheavyDict}
@@ -65,7 +66,7 @@ def getStats(filename):
     return resultDict
 
 
-def setGraphData(dictionary, axs, xValues, yValues):
+def setGraphData(dictionary, axs, xValues, yValues, format = False):
 
     legends = ['FIFO', 'Heavy', 'Thread', 'Pre-Heavy']
     fifoXValues = dictionary["data"]["fifoDict"][xValues]
@@ -88,6 +89,10 @@ def setGraphData(dictionary, axs, xValues, yValues):
     axs[dictionary["posX"], dictionary["posY"]].set_title(dictionary["title"])
     axs[dictionary["posX"], dictionary["posY"]].legend(legends, loc='upper right')  # set the position of the legend
     axs[dictionary["posX"], dictionary["posY"]].set(xlabel=dictionary["xLabel"], ylabel=dictionary["yLabel"])
+
+    # formatear el eje y para mostrar los números en millones
+    if format:
+        axs[dictionary["posX"], dictionary["posY"]].get_yaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: (str(x/1000) + "k")))
 
 
 def drawStats():
@@ -122,14 +127,14 @@ def drawStats():
     graph4Dict = {"posX": 1, "posY": 1, "x": x + 2, "y": y + 2,
                   "title": "Data Transfer Speed",
                   "xLabel": "timeExecution", "yLabel": "totalBytesSend",
-                  "xValues": "timeExecution",
+                  "xValues": "times",
                   "yValues": "totalBytesSend",
                   "data": timeRequest}
 
     setGraphData(graph1Dict, axs, graph1Dict["xValues"], graph1Dict["yValues"])
     setGraphData(graph2Dict, axs, graph2Dict["xValues"], graph2Dict["yValues"])
     setGraphData(graph3Dict, axs, graph3Dict["xValues"], graph3Dict["yValues"])
-    setGraphData(graph4Dict, axs, graph4Dict["xValues"], graph4Dict["yValues"])
+    setGraphData(graph4Dict, axs, graph4Dict["xValues"], graph4Dict["yValues"], True)
 
     fig.tight_layout()
     plt.show()
